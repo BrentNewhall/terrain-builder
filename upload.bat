ace51d09d11b6acc4503d603a145eb4a943c1f56
Brent P. Newhall <brent@brentnewhall.com>
2019-04-10 21:56:26 -0400

    Change upload.bat to point to ga; widen sidebar

--- upload.bat
+++ upload.bat
@@ -1,4 +1,7 @@
 @echo off
 
-CALL aws s3 cp index.html s3://terrain-builder/
-CALL aws s3 cp style.css s3://terrain-builder/
+REM CALL aws s3 cp index.html s3://terrain-builder/
+REM CALL aws s3 cp style.css s3://terrain-builder/
+
+scp -i "C:\Users\Brent Newhall\Documents\certs\brent-amazon.pem" index.html ec2-user@geekarchaeology.com:/var/www/html/terrain-builder/
+scp -i "C:\Users\Brent Newhall\Documents\certs\brent-amazon.pem" style.css ec2-user@geekarchaeology.com:/var/www/html/terrain-builder/
