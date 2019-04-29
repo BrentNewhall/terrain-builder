0a46d132c9ff045492c6832ee90fb2141057f074
Brent P. Newhall <brent@brentnewhall.com>
2019-04-28 20:53:40 -0400

    Update upload.bat for new js file location

--- upload.bat
+++ upload.bat
@@ -4,5 +4,5 @@ REM CALL aws s3 cp index.html s3://terrain-builder/
 REM CALL aws s3 cp style.css s3://terrain-builder/
 
 scp -i "C:\Users\Brent Newhall\Documents\certs\brent-amazon.pem" index.html ec2-user@geekarchaeology.com:/var/www/html/terrain-builder/
-scp -i "C:\Users\Brent Newhall\Documents\certs\brent-amazon.pem" main.js ec2-user@geekarchaeology.com:/var/www/html/terrain-builder/
+scp -i "C:\Users\Brent Newhall\Documents\certs\brent-amazon.pem" js/main.js ec2-user@geekarchaeology.com:/var/www/html/terrain-builder/js/
 scp -i "C:\Users\Brent Newhall\Documents\certs\brent-amazon.pem" style.css ec2-user@geekarchaeology.com:/var/www/html/terrain-builder/
